ded2791ba73ae172ec8145644487bc91b5e4da97
ClsHdz <carlos.hdz97@gmail.com>
2015-11-13 16:54:00 +0100

    Terminado Ejercicio 6

---
+++ src/Repaso6.java
@@ -0,0 +1,27 @@
+import acm.program.*;
+import acm.util.RandomGenerator;
+import acm.graphics.*;
+
+import java.awt.event.MouseEvent;
+public class Repaso6 extends GraphicsProgram {
+	GRect rectangulo;
+	RandomGenerator aleatorio = new RandomGenerator();
+	int distanciaX;
+	int distanciaY;
+	public void init(){
+		setSize(800,600);
+		rectangulo = new GRect(120,80);
+		addMouseListeners();
+	}
+	public void run(){
+		distanciaX = getWidth()/2;
+		distanciaY = getHeight()/2;
+		add(rectangulo,distanciaX - (rectangulo.getWidth()/2), distanciaY - (rectangulo.getHeight()/2));
+	}
+	public void mouseClicked(MouseEvent evento){
+		if(getElementAt(evento.getX(),evento.getY()) == rectangulo){
+			rectangulo.setFilled(true);
+			rectangulo.setFillColor(aleatorio.nextColor());
+		}
+	}
+}
