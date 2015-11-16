b476252bd32329300cb4e670059207640775ed6f
ClsHdz <carlos.hdz97@gmail.com>
2015-11-16 20:02:36 +0100

    Aclaraciones de cuando empieza cada ejercicio.

--- src/EjerciciosBasicosJava.java
+++ src/EjerciciosBasicosJava.java
@@ -5,21 +5,21 @@ public class EjerciciosBasicosJava extends ConsoleProgram{
 	boolean respuesta;
 	int solucion;
 	public void run(){
-//		respuesta = fiestaArdillas(70,true);	
+//		respuesta = fiestaArdillas(70,true);
 //		println(""+respuesta+"");
 //		solucion = multa(85,true);
 //		println(""+solucion+"");
 //		respuesta = muyVanidoso(24);
 //		println(""+respuesta+"");
-//		respuesta = contesta(true,false,false);	
+//		respuesta = contesta(true,false,false);
 //		println(""+respuesta+"");
-//		respuesta = menorPor10(2,11,1);	
+//		respuesta = menorPor10(2,11,1);
 //		println(""+respuesta+"");
-//		respuesta = digitoIgual(12,44);	
+//		respuesta = digitoIgual(12,44);	 
 //		println(""+respuesta+"");
-//		respuesta = multiploMultiple(7);	
+//		respuesta = multiploMultiple(7);
 //		println(""+respuesta+"");
-//		respuesta = menos20(40);	
+//		respuesta = menos20(40);
 //		println(""+respuesta+"");
 //		solucion = loteria(2,2,0);
 //		println(""+solucion+"");
@@ -27,7 +27,7 @@ public class EjerciciosBasicosJava extends ConsoleProgram{
 		println(""+solucion+"");
 	}
 	
-	private boolean fiestaArdillas(int n ,boolean finde){
+	private boolean fiestaArdillas(int n ,boolean finde){				//Ejercicio 1.
 		if(finde == false){
 			if(n < 60 && n > 30){
 				return true;
@@ -40,7 +40,7 @@ public class EjerciciosBasicosJava extends ConsoleProgram{
 			return true;
 		}
 	}
-	private int multa(int n, boolean c){
+	private int multa(int n, boolean c){								//Ejercicio 2.
 		if(c == true){
 			n = n -5;
 		}
@@ -56,7 +56,7 @@ public class EjerciciosBasicosJava extends ConsoleProgram{
 			}
 		}
 	}
-	private boolean muyVanidoso(int n){
+	private boolean muyVanidoso(int n){									//Ejercicio 3.
 		if(n % 11 == 0 || n % 11 == 1){
 			return true;
 		}
@@ -64,7 +64,7 @@ public class EjerciciosBasicosJava extends ConsoleProgram{
 			return false;
 		}
 	}
-	private boolean contesta(boolean matinal, boolean madre, boolean dormido){
+	private boolean contesta(boolean matinal, boolean madre, boolean dormido){		//Ejercicio 4.
 		if(dormido == true){
 			return false;
 		}
@@ -82,7 +82,7 @@ public class EjerciciosBasicosJava extends ConsoleProgram{
 			}
 		}
 	}
-	private boolean menorPor10(int numero, int cifra, int n){
+	private boolean menorPor10(int numero, int cifra, int n){			//Ejercicio 5.
 		if(numero - cifra >= 10 || numero - n >= 10 || n - numero >= 10 || cifra - numero >= 10 || n - cifra >= 10 || cifra - n >= 10){
 			return true;
 		}
@@ -90,7 +90,7 @@ public class EjerciciosBasicosJava extends ConsoleProgram{
 			return false;
 		}
 	}
-	private boolean digitoIgual(int n, int cifra){
+	private boolean digitoIgual(int n, int cifra){						//Ejercicio 6.
 		int num1 = n/10;
 		int num2 = n%10;
 		int num3 = cifra/10;
@@ -102,7 +102,7 @@ public class EjerciciosBasicosJava extends ConsoleProgram{
 			return false;
 		}
 	}
-	private boolean multiploMultiple(int n){
+	private boolean multiploMultiple(int n){							//Ejercicio 7.
 		if(n % 3 == 0 || n % 5 == 0){
 			if(n % 3 == 0 && n % 5 == 0){
 				return false;
@@ -115,7 +115,7 @@ public class EjerciciosBasicosJava extends ConsoleProgram{
 			return false;
 		}
 	}
-	private boolean menos20(int n){
+	private boolean menos20(int n){										//Ejercicio 8.
 		if(n % 20 == 18 || n % 20 == 19){
 			return true;
 		}
@@ -123,7 +123,7 @@ public class EjerciciosBasicosJava extends ConsoleProgram{
 			return false;
 		}
 	}
-	private int loteria(int a, int b, int c){
+	private int loteria(int a, int b, int c){							//Ejercicio 9.
 		if(a == b && b == c){
 			if(a == 2){
 				return 10;
@@ -141,7 +141,7 @@ public class EjerciciosBasicosJava extends ConsoleProgram{
 			}
 		}
 	}
-	private int withoutDoubles(int n, int i, boolean doble){
+	private int withoutDoubles(int n, int i, boolean doble){			//Ejercicio 10.
 		int j = 0;
 		if(doble == true && n == i){
 			j = 1;
