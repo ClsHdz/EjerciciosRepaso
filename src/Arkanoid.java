110606a0df3541a5971b5022ba75251193b2e618
ClsHdz <carlos.hdz97@gmail.com>
2015-12-01 16:56:25 +0100

    Añadida la posibilidad de pulsar la barra espaciadora para pausar.

--- src/Arkanoid.java
+++ src/Arkanoid.java
@@ -31,6 +31,7 @@ public class Arkanoid extends GraphicsProgram{
 	int cuentaLadrillos = 0;
 	int score = 0;
 	int n;
+	boolean pausa = false;
 	boolean choqueDoble = false;
 	boolean choquePlataforma = false;
 	boolean choqueLadrillo = false;
@@ -121,147 +122,151 @@ public class Arkanoid extends GraphicsProgram{
 		remove(Modo2);
 		gameOver = false;
 		while(gameOver == false){
-			if(velocidadX > 6){
-				velocidadX = 6;
-			}
-			if(velocidadX < -6){
-				velocidadX = -6;
-			}
-			if(velocidadY > 12){
-				velocidadY = 12;
-			}
-			if(velocidadY < -12){
-				velocidadY = -12;
-			}
-			RandomGenerator aleatorio = new RandomGenerator();
-			n = aleatorio.nextInt(100);
-			puntJuego = new GLabel(""+score+"");
-			puntJuego.setLocation(0, 800);
-			add(puntJuego);
-			choqueDoble = false;
-			choquePlataforma = false;
-			choqueLadrillo = false;
-			choqueUpLeft = false;
-			choqueUpRight = false;
-			choqueDownLeft = false;
-			choqueDownRight = false;
-			if(inicio == false){
-				bola.setLocation(plataforma.getX(), plataforma.getY() - 15);
-			}
-			else{
-				bola.move(velocidadX, -velocidadY);
-			}
-			pause(30);
-			if(bola.getX() + 25 > ANCHO_PANTALLA){
-				velocidadX = velocidadX * -1;
-			}
-			if(bola.getX() < 0){
-				velocidadX = velocidadX * -1;
-			} 
-			GObject choque = null;
-			double posX = bola.getX();
-			double posY = bola.getY();
-			choque = getElementAt(posX,posY);
-			if(choque != null && choque != bola && choque != plataforma && choqueLadrillo == false){
-				remove(choque);
-				cuentaLadrillos++;
-				score = score + 100;
-				choqueUpLeft = true;
-				if(posX >= choque.getX() + ladrillo.getWidth() -2 && posY >= choque.getY()){
-					velocidadX = velocidadX * -1;
+			while(pausa == false){
+				if(velocidadX > 6){
+					velocidadX = 6;
+				}
+				if(velocidadX < -6){
+					velocidadX = -6;
+				}
+				if(velocidadY > 12){
+					velocidadY = 12;
+				}
+				if(velocidadY < -12){
+					velocidadY = -12;
+				}
+				RandomGenerator aleatorio = new RandomGenerator();
+				n = aleatorio.nextInt(100);
+				puntJuego = new GLabel(""+score+"");
+				puntJuego.setLocation(0, 800);
+				add(puntJuego);
+				choqueDoble = false;
+				choquePlataforma = false;
+				choqueLadrillo = false;
+				choqueUpLeft = false;
+				choqueUpRight = false;
+				choqueDownLeft = false;
+				choqueDownRight = false;
+				if(inicio == false){
+					bola.setLocation(plataforma.getX(), plataforma.getY() - 15);
 				}
 				else{
-					velocidadY = velocidadY * -1;
+					bola.move(velocidadX, -velocidadY);
 				}
-				choqueLadrillo = true;
-				
-			}
-			choque = getElementAt(posX,posY + bola.getHeight());
-			if(choque == plataforma && choquePlataforma == false){
-				velocidadY = velocidadY * (-1);
-				bola.setLocation(bola.getX(), bola.getY()-15);
-				choquePlataforma = true;
-			}
-			else{
+				pause(30);
+				if(bola.getX() + 25 > ANCHO_PANTALLA){
+					velocidadX = velocidadX * -1;
+				}
+				if(bola.getX() < 0){
+					velocidadX = velocidadX * -1;
+				} 
+				GObject choque = null;
+				double posX = bola.getX();
+				double posY = bola.getY();
+				choque = getElementAt(posX,posY);
 				if(choque != null && choque != bola && choque != plataforma && choqueLadrillo == false){
 					remove(choque);
 					cuentaLadrillos++;
 					score = score + 100;
-					choqueDownLeft = true;
-					if(posX >= choque.getX() + ladrillo.getWidth() -2 && posY >= choque.getY()){
+					choqueUpLeft = true;
+					if(posX >= choque.getX() + ladrillo.getWidth() -1 && posY >= choque.getY() +1 && posY <= choque.getY() + choque.getHeight() -1){
 						velocidadX = velocidadX * -1;
 					}
 					else{
 						velocidadY = velocidadY * -1;
 					}
 					choqueLadrillo = true;
-					
-				}
-			}
-			choque = getElementAt(posX + bola.getWidth(),posY);
-			if(choque != null && choque != bola && choque != plataforma && choqueLadrillo == false){
-				remove(choque);
-				cuentaLadrillos++;
-				score = score + 100;
-				choqueUpRight = true;
-				if(posX <= choque.getX() + 2 && posY >= choque.getY()){
-					velocidadX = velocidadX * -1;
-				}
-				else{
-					velocidadY = velocidadY * -1;
-				}
-				choqueLadrillo = true;
-				
-			}
 
-			choque = getElementAt(posX + bola.getWidth(),posY + bola.getHeight());
-			if(choque == plataforma && choquePlataforma == false){
-				velocidadY = velocidadY * (-1);
-				bola.setLocation(bola.getX(), bola.getY()-15);
-				choquePlataforma = true;
-			}
-			else{
+				}
+				choque = getElementAt(posX,posY + bola.getHeight());
+				if(choque == plataforma && choquePlataforma == false){
+					velocidadY = velocidadY * (-1);
+					bola.setLocation(bola.getX(), bola.getY()-15);
+					choquePlataforma = true;
+				}
+				else{
+					if(choque != null && choque != bola && choque != plataforma && choqueLadrillo == false){
+						remove(choque);
+						cuentaLadrillos++;
+						score = score + 100;
+						choqueDownLeft = true;
+						if(posX >= choque.getX() + ladrillo.getWidth() -1 && posY >= choque.getY() +1 && posY <= choque.getY() + choque.getHeight() -1){
+							velocidadX = velocidadX * -1;
+						}
+						else{
+							velocidadY = velocidadY * -1;
+						}
+						choqueLadrillo = true;
+
+					}
+				}
+				choque = getElementAt(posX + bola.getWidth(),posY);
 				if(choque != null && choque != bola && choque != plataforma && choqueLadrillo == false){
 					remove(choque);
 					cuentaLadrillos++;
 					score = score + 100;
-					choqueDownRight = true;
-					if(posX <= choque.getX() + 2 && posY >= choque.getY()){
+					choqueUpRight = true;
+					if(posX <= choque.getX() + 1 && posY >= choque.getY() +1 && posY <= choque.getY() + choque.getHeight() -1){
 						velocidadX = velocidadX * -1;
 					}
 					else{
 						velocidadY = velocidadY * -1;
 					}
 					choqueLadrillo = true;
-					
+
 				}
-			}
-			if(bola.getY() < 0){
-				velocidadY = velocidadY * -1;
-			}
-			if(bola.getY() + 15 > ALTO_PANTALLA){
-				gameOver = true;
-			}
-			if(cuentaLadrillos == 27){
-				if(velocidadX > 0){
-					velocidadX = velocidadX + 1;
+
+				choque = getElementAt(posX + bola.getWidth(),posY + bola.getHeight());
+				if(choque == plataforma && choquePlataforma == false){
+					velocidadY = velocidadY * (-1);
+					bola.setLocation(bola.getX(), bola.getY()-15);
+					choquePlataforma = true;
 				}
 				else{
-					velocidadX = velocidadX - 1;
+					if(choque != null && choque != bola && choque != plataforma && choqueLadrillo == false){
+						remove(choque);
+						cuentaLadrillos++;
+						score = score + 100;
+						choqueDownRight = true;
+						if(posX <= choque.getX() + 1 && posY >= choque.getY() +1 && posY <= choque.getY() + choque.getHeight() -1){
+							velocidadX = velocidadX * -1;
+						}
+						else{
+							velocidadY = velocidadY * -1;
+						}
+						choqueLadrillo = true;
+
+					}
 				}
-				if(velocidadY > 0){
-					velocidadY = velocidadY + 2;
+				if(bola.getY() < 0){
+					velocidadY = velocidadY * -1;
 				}
-				else{
-					velocidadY = velocidadY - 2;
+				if(bola.getY() + 15 > ALTO_PANTALLA){
+					gameOver = true;
+					pausa = true;
 				}
+				if(cuentaLadrillos == 27){
+					if(velocidadX > 0){
+						velocidadX = velocidadX + 1;
+					}
+					else{
+						velocidadX = velocidadX - 1;
+					}
+					if(velocidadY > 0){
+						velocidadY = velocidadY + 2;
+					}
+					else{
+						velocidadY = velocidadY - 2;
+					}
+				}
+				if(cuentaLadrillos == 54){
+					gameOver = true;
+					ganador = true;
+					pausa = true;
+				}
+				remove(puntJuego);
+
 			}
-			if(cuentaLadrillos == 54){
-				gameOver = true;
-				ganador = true;
-			}
-			remove(puntJuego);
-			
 		}
 		if(modo1 == false){
 			add(bola);
@@ -294,11 +299,17 @@ public class Arkanoid extends GraphicsProgram{
 			modo2 = true;
 			modo = true;
 		}
+		if(pausa == true){
+			pausa = false;
+		}
 	}
 	public void keyPressed (KeyEvent eventos){
 		if(eventos.getKeyCode() == KeyEvent.VK_9){
 			ganador = true;
 			gameOver = true;
+		}
+		if(eventos.getKeyCode() == KeyEvent.VK_SPACE){
+			pausa = true;
 		}
 
 	}
